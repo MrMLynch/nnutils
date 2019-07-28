a259b62c45c7476cfac921c8d0014ad51d659717
MrMLynch <mrlynch@protonmail.ch>
2019-07-29 00:06:42 +0100

    Update checkconn.sh

--- checkconn.sh
+++ checkconn.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-nodes=$(netstat -tenpula | grep 17776 | awk '{print $5}' | sort -n | sed 's/\:.*$//' | uniq)
+nodes=$(curl -s --url "http://127.0.0.1:7779" --data "{\"agent\":\"dpow\",\"method\":\"ipaddrs\"}" | jq -r .[])
+timedate=$(date -u +'%d/%m/%Y %H:%M:%S')
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 for node in $nodes
 do
   if netcat -z -w 2 $node 17775 2>/dev/null; then
-    echo "$node ✓"
+    printf "[$timedate] ${GREEN}$node -  ^|^s${NC}\n"
   else
-    echo "$node ✗"
+    printf "[$timedate] ${RED}$node -  ^|^w${NC}\n" | tee -a notcool.bro
   fi
 done
